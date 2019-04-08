4b2e621f86cc570d8862a83727cbb661f52f42cd
ltybc157 <den.ste21013@yandex.ru>
2019-04-09 00:21:55 +0600

    коммит  lesson 16

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Clock from './App';
 import * as serviceWorker from './serviceWorker';
+import Button from './Button' 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    render() {
+        return (
+            <div className="wrapper">
+            <Clock/>
+            <Button/>
+            </div>
+        )
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+/*function Greet(props) {// прописываем функцию ,чтобы что-то вывелолось на странице ,название функции пишем с большой буквы
+  // let phrase = "World!";//прописываем переменную с значением строки  World!
+    return(
+    <h1>Hello {props.phrase}! My name is {props.name}</h1>// в {} скобки можно записывать выражение, переменные ,функции и т.д
+)
+}
+function GreeatAll() {
+    return (
+        <div>
+            <Greet phrase="World!" name="Ival"/>
+            <Greet phrase="World!" name="Anna"/>
+            <Greet phrase="World!" name="Alex"/>
+        </div>
+    )
+}  закомментировал 17:00*/
+ReactDOM.render(<App/> , document.getElementById('root'));
 serviceWorker.unregister();
